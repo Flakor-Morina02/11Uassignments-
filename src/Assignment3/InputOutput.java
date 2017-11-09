2793109f89267c722c6f2af69212f141c8d8a43f
morif9929 <morif9929@HRH-IC0027326.wrdsb.ca>
2017-11-09 13:38:14 -0500

    All of Assignments

---
+++ src/Assignment3/InputOutput.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author morif9929
+ */
+public class InputOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    //say hello
+        System.out.println("Hello world!");
+        System.out.print("Insert user name");
+        System.out.print("\n");
+
+    //name variable
+        String firstName = "Flakor";
+        
+        Scanner input = new Scanner(System.in);
+        
+      
+        
+        
+       
+                
+        //output the variable
+        System.out.println("Hello" + firstName);
+        
+                
+                int birthyear =input.nextInt();
+                
+              
+                
+                
+    }
+}
